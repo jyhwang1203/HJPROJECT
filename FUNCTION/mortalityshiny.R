c1e665fc5d9594b6af6a6a6bbe0cc62385f69739
jyhwang1203 <131414692+jyhwang1203@users.noreply.github.com>
2024-01-02 17:15:09 +0900

    RR
    TT

---
+++ FUNCTION/mortalityshiny.R
@@ -0,0 +1,95 @@
+ALPHA <- data.frame(STD_DT=LCfit$ages ,TOTAL= LCfit$ax%>%as.numeric,MALE=LCfitm$ax%>%as.numeric,FEMALE=LCfitf$ax%>%as.numeric)
+data1 <- LCfit$kt%>%t%>%ts(frequency = 1,start=c(1970),end=c(2021))
+data2 <- LCfitm$kt%>%t%>%ts(frequency = 1,start=c(1970),end=c(2021))
+data3 <- LCfitf$kt%>%t%>%ts(frequency = 1,start=c(1970),end=c(2021))
+data.frame(STD_DT=c(1970:2021), TOTAL=cusum(data1),MALE=cusum(data2),FEMALE=cusum(data3))
+data.frame(STD_DT=c(1970:2071), TOTAL=c(LCfit$kt,LCfor$kt.f$mean),MALE=c(LCfitm$kt,LCform$kt.f$mean),FEMALE=c(LCfitf$kt,LCforf$kt.f$mean))%>%cplot
+
+TMP <- TXT %>% melt %>% left_join(MXT %>% melt,by=c("X1","X2"))%>% left_join(FXT %>% melt,by=c("X1","X2"))
+
+TMP <- TXT %>% melt %>% left_join(RTXT %>% melt,by=c("X1","X2"))
+TMP <- MXT %>% melt %>% left_join(RMXT %>% melt,by=c("X1","X2"))
+TMP <- FXT %>% melt %>% left_join(RFXT %>% melt,by=c("X1","X2"))
+
+TMP <-  RTXT %>% melt %>% left_join(RFXT %>% melt,by=c("X1","X2"))%>% left_join(RMXT %>% melt,by=c("X1","X2"))
+TMP %>% dplyr::filter(X1=="60")%>%.[,-1]%>%rename(c(X2="STD_DT"))%>%cplot
+
+mxm <- BASEKRM$Dxt/BASEKRM$Ext
+mxf <- BASEKRF$Dxt/BASEKRF$Ext
+mxm_llg <- exp(LCfitm$bx%>%as.matrix()%*%kappa_fv$`Li-LEE-MALE`+matrix(data = (LCfitm$ax)%>%as.matrix(), nrow = 100, ncol = 10))%>%round(5)
+mxf_llg <- exp(LCfitf$bx%>%as.matrix()%*%kappa_fv$`Li-LEE-FEMALE`+matrix(data = (LCfitf$ax)%>%as.matrix(), nrow = 100, ncol = 10))%>%round(5)
+mxm_ll  <- exp(LCfitm$bx%>%as.matrix()%*%kappa_fv$`LC-MALE`+matrix(data = (LCfitm$ax)%>%as.matrix(), nrow = 100, ncol = 10))%>%round(5)
+mxf_ll  <- exp(LCfitf$bx%>%as.matrix()%*%kappa_fv$`LC-FEMALE`+matrix(data = (LCfitf$ax)%>%as.matrix(), nrow = 100, ncol = 10))%>%round(5)
+
+cbind(STD_DT=c(2012:2021),mxm[45,c(2012:2021)%>%as.character()],mxm_ll[45,],mxm_llg[45,])%>%as.data.frame%>%cplot
+
+    ui <- navbarPage("LI-LEE류",theme = shinytheme("flatly"),
+       tabPanel("기대여명",dashboardPage(dashboardHeader(),
+        dashboardSidebar(
+         dateRangeInput('range',label = '',start = as.Date('2020-01-01') , end = as.Date('2023-12-31')),
+         textInput("age", "AGE")),
+         dashboardBody(
+           fluidRow(shinydashboard::valueBox("Lee-Carter","모수정정",color="red", width = 12)),
+           fluidRow(
+             box(plotOutput("ALPHA"), width =6, solidHeader = TRUE),
+             box(plotOutput("BETA") , width =6, solidHeader = TRUE),
+                                ),
+           fluidRow(
+             box(plotOutput("KAPPA"), width =6, solidHeader = TRUE),
+             box(plotOutput("CUSUM"), width =6, solidHeader = TRUE)),
+           fluidRow(shinydashboard::valueBox("Lee-Carter","미래예측",color="red", width = 12)),
+           fluidRow(
+             box(plotOutput("KAPPAVF"), width =6, solidHeader = TRUE),
+             box(plotOutput("MORVF"), width =6, solidHeader = TRUE)),
+           fluidRow(
+             box(DTOutput("MSE"), width  =4, solidHeader = TRUE),
+             box(DTOutput("RMSE"), width =4, solidHeader = TRUE),
+             box(DTOutput("MAPE"), width =4, solidHeader = TRUE))
+           
+                      ))))
+    
+    
+   server <- function(input, output){   
+        output$KAPPA <- renderPlot({
+        KT <- data.frame(STD_DT=LCfit$years,TOTAL=LCfit$kt%>%as.numeric,MALE=LCfitm$kt%>%as.numeric,FEMALE=LCfitf$kt%>%as.numeric)
+        KT %>% cplot(.,"KAPPA")
+        })
+        output$ALPHA <- renderPlot({
+        ALPHA <- data.frame(STD_DT=LCfit$ages ,TOTAL= LCfit$ax%>%as.numeric,MALE=LCfitm$ax%>%as.numeric,FEMALE=LCfitf$ax%>%as.numeric)
+        ALPHA %>% cplot(.,"ALPHA")
+        })
+        output$BETA <- renderPlot({
+        BETA <- data.frame(STD_DT=LCfit$ages ,TOTAL= LCfit$bx%>%as.numeric,MALE=LCfitm$bx%>%as.numeric,FEMALE=LCfitf$bx%>%as.numeric)
+        BETA %>% cplot(.,"BETA")
+        })
+        output$CUSUM <- renderPlot({
+        data.frame(STD_DT=c(1970:2021), TOTAL=cusum(data1),MALE=cusum(data2),FEMALE=cusum(data3)) %>% cplot(.,"CUSUM STATISTIC")
+        })
+        output$KAPPAF <- renderPlot({
+        data.frame(STD_DT=c(1970:2071), TOTAL=c(LCfit$kt,LCfor$kt.f$mean),MALE=c(LCfitm$kt,LCform$kt.f$mean),FEMALE=c(LCfitf$kt,LCforf$kt.f$mean))%>%
+            cplot(.,"KAPPAF")
+        })
+        output$KAPPAVF <- renderPlot({
+          kappa_fv%>%cplot(.,"Kappa")
+        })
+        output$MORVF <- renderPlot({
+          cbind(STD_DT=c(2012:2021),REAL=mxm[input$age,c(2012:2021)%>%as.character()],LC=mxm_ll[input$age,],
+                LLG=mxm_llg[input$age,],LSTM=mxm_lstm[input$age,])%>%as.data.frame%>%
+            cplot(.,"사망률 예측치(2012~2021)")
+        })
+        output$MSE <- renderDT({
+          data.frame(rbind(MSEM,MSEF))
+        })       
+        output$RMSE <- renderDT({
+          data.frame(rbind(RMSEM,RMSEF)) 
+        })     
+        output$MAPE <- renderDT({
+          data.frame(rbind(MAPEM,MAPEF))
+        })     
+        }
+
+shinyApp(ui, server)
+
+    KT%>%reshape2::melt(id.vars = "STD_DT")%>%na.omit%>%
+  ggplot(aes(x=STD_DT, y=value, col = variable,fill=variable)) +             
+  geom_line(size=1)
