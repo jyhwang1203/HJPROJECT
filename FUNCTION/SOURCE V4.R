96f41ef2534090bfd1d1c12c385803085e91bf7c
jyhwang1203 <131414692+jyhwang1203@users.noreply.github.com>
2024-02-02 16:44:36 +0900

    DD
    DD

--- FUNCTION/SOURCE V4.R
+++ FUNCTION/SOURCE V4.R
@@ -16,13 +16,12 @@ remove.packages("bvartools")
       CLI  <-  read.csv("c:/work/CLI.csv",stringsAsFactors = FALSE,header=T)
       CLI  <- dcast(CLI,TIME ~LOCATION,  value.var = "Value")
       CLI  <- CLI %>% as.data.frame() %>% mutate(STD_DT=paste0(CLI$TIME,"-01") %>%ymd+months(1)-days(1))
-     
+      RAWDATA <- read.csv("c:/work/RAWDATA2024V1.csv",stringsAsFactors = FALSE)%>%dplyr::select(-X)%>%mutate(STD_DT=as.Date(STD_DT))%>%as.data.table%>%na.omit
       RAWDATA5<- readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="FEPS")
       RAWDATA6<- readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="TPER")
       colnames(RAWDATA5) <- paste0("FEPS_",colnames(RAWDATA5))
       colnames(RAWDATA6) <- paste0("TPER_",colnames(RAWDATA6))
       TMP <- rbind(
-     # RAWDATA%>%na.omit
        readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="EQ")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       ,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="AL")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       ,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="FX")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
@@ -31,18 +30,23 @@ remove.packages("bvartools")
       ,RAWDATA6%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       ,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="RATE")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       ,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="RISK")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit%>%filter(variable!="HYSP")
-      #,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="ETF") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       ,readxl::read_excel("c:/work/RAWDATA2024.xlsx",sheet="FORECAST")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
-      ,read.csv("c:/work/RAWDATA2024-01.csv",stringsAsFactors = FALSE)%>%dplyr::select(-X)%>%mutate(STD_DT=as.Date(STD_DT))%>%as.data.table%>% filter(STD_DT<"2024-01-01")%>%na.omit
+      ,readxl::read_excel("c:/work/MACROH.xlsx",sheet="weekly") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
+      ,RAWDATA
       
-      #,readxl::read_excel("c:/work/STOCKINDEX.xlsx",sheet="Sheet1")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
       )%>%as.data.table%>%mutate(variable==as.character(variable))
-      write.csv(TMP ,"c:/work/RAWDATA2024-03.csv")
-      #readxl::read_excel("c:/work/STOCKINDEX.xlsx",sheet="Sheet1")%>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit%>%dcast(STD_DT~variable)
+      write.csv(TMP ,"c:/work/RAWDATA2024V2.csv")
+      RAWDATA <- read.csv("c:/work/RAWDATA2024V2.csv",stringsAsFactors = FALSE)%>%dplyr::select(-X)%>%mutate(STD_DT=as.Date(STD_DT))%>%as.data.table%>%na.omit
       
-      RAWDATA <- read.csv("c:/work/RAWDATA2024-01.csv",stringsAsFactors = FALSE)%>%dplyr::select(-X)%>%mutate(STD_DT=as.Date(STD_DT))%>%as.data.table%>%na.omit
-      #RAWDATA <- RAWDATA %>% filter(STD_DT<"2024-01-01")
-      #write.csv(RAWDATA ,"c:/work/RAWDATA2024-02.csv")
-      # RAWDATA <- RAWDATA%>%filter(variable!="MSTW"&variable!="MSJP"&variable!="MSIN"&variable!="MSDE"&variable!="MSFR")
-      RAWDATA%>%filter(variable=="US10Y")%>%dcast(STD_DT~variable)%>%arrange(STD_DT)
-     
+
+      RAWDATA%>%filter(variable=="WORLDT")%>%dcast(STD_DT~variable)%>%arrange(STD_DT,decreasing=T)
+      TMP<- rbind(
+      TMP,
+      readxl::read_excel("c:/work/RAWDATATMP.xlsx",sheet="Sheet1") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
+      )
+      TMP <- rbind(
+      readxl::read_excel("c:/work/MACROH.xlsx",sheet="weekly") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
+      ,readxl::read_excel("c:/work/MACROH.xlsx",sheet="monthly") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
+      ,readxl::read_excel("c:/work/MACROH.xlsx",sheet="quarter") %>%handling%>%as.data.table%>%melt(id.vars="STD_DT")%>%na.omit
+      )%>%filter(STD_DT>"2022-12-31")
+TMP <- rbind(RAWDATA,TMP)
